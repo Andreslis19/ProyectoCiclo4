9f3e6f3baa94afc08478f3f6aaee9cd4e4a918c0
AlejoHerrera <alejoherrera695@gmail.com>
2021-11-28 16:21:42 -0500

    Materialize
    instalacion del framework materialize, creacion de plantillas de pie de pagina y barra de navegacion. ingreo a los modulos y el error en cuando colocan un error en la url

--- Angular/frontendAngular/src/app/app-routing.module.ts
+++ Angular/frontendAngular/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
+import { ErrorComponent } from './plantilla/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "inicio",
+    component: InicioComponent
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/inicio"
+  },
+  {
+    path: "seguridad",
+    loadChildren: ()=>import("./modulos/seguridad/seguridad.module").then(x => x.SeguridadModule)
+  },
+  {
+    path: "administracion",
+    loadChildren: ()=>import("./modulos/administracion/administracion.module").then(x => x.AdministracionModule)
+  },
+  {
+    path: "pedidos",
+    loadChildren: ()=>import("./modulos/compra/compra.module").then(x => x.CompraModule)
+  },
+  {
+    path: "**",  //sintaxis para los comodines en caso de que coloque mal la url
+    component: ErrorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
