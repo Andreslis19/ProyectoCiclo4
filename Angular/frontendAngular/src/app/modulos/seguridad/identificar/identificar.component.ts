dbac0bc500eeae704531fa03993734be29395f19
AlejoHerrera <alejoherrera695@gmail.com>
2021-12-01 23:37:37 -0500

    materialize
    creacion de formulario de inicio de sesion

---
+++ Angular/frontendAngular/src/app/modulos/seguridad/identificar/identificar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-identificar',
+  templateUrl: './identificar.component.html',
+  styleUrls: ['./identificar.component.css']
+})
+export class IdentificarComponent implements OnInit {
+
+  fgValidador : FormGroup = this.fb.group({
+    'usuario' : ['', [Validators.required, Validators.email]],
+    'clave' : ['', [Validators.required]]
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  IdentificarUsuario(){
+    let usuario = this.fgValidador.controls['usuario'].value;
+    let clave = this.fgValidador.controls['clave'].value;
+
+    alert(usuario);
+    alert(clave);
+  }
+
+}
--- Angular/frontendAngular/src/app/modulos/seguridad/seguridad.module.ts
+++ Angular/frontendAngular/src/app/modulos/seguridad/seguridad.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SeguridadRoutingModule } from './seguridad-routing.module';
+import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
+import { CerrarSesionComponent } from './cerrar-sesion/cerrar-sesion.component';
+import { IdentificarComponent } from './identificar/identificar.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CambioClaveComponent,
+    CerrarSesionComponent,
+    IdentificarComponent,
+    RecuperarClaveComponent
+  ],
   imports: [
     CommonModule,
-    SeguridadRoutingModule
+    SeguridadRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SeguridadModule { }
